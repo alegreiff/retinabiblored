a213dd1f940169e6f7c3db800dc49a5e6d01730f
Jaime de Greiff <alegreiff@gmail.com>
2024-09-25 21:40:48 -0500

    podcast

--- src/interfaces/retina.ts
+++ src/interfaces/retina.ts
@@ -21,3 +21,26 @@ export interface Imagenes {
   destacada: string
   poster: string
 }
+
+export interface Podcast {
+  nombre: string
+  tipo_de_contenido: string
+  directores: string[]
+  fecha: string
+  publicador: string
+  lugar: string
+  idioma: string
+  duracion: string
+  descripcion: string
+  tags: string[]
+  licencia: string
+  enlace: string
+  imagenes: Imagenes2
+  temporada: string
+  episodio: string
+  slug: any
+}
+
+export interface Imagenes2 {
+  destacada: string
+}
